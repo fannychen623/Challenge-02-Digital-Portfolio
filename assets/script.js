b13c97dfedeba16e3d19ea85185584dff71887a4
fycn <fannychen623@gmail.com>
2022-11-17 20:29:55 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
-let mybutton = document.getElementById("top-button");
+// definition and function for button to scroll to the top of the page
 
+let mybutton = document.getElementById("top-button");
 window.onscroll = function() {scrollFunction()};
 
+// display button when scrolled past top
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -10,6 +12,7 @@ function scrollFunction() {
   }
 }
 
+// function on click
 function topFunction() {
     document.documentElement.scrollTop = 0;
 }
